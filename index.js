cd9961ebdbe5cc6aca23cf9504eb23f453d51746
wuyong <biggodw@gmail.com>
2023-11-02 01:42:01 +0800

    render

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express()
 const router = require('./router/index.js')
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/',(req,res)=>{
+    res.render('./public/index.html')
+})
 app.use('/api',router)
 
 
-app.listen(3000,()=>{
+app.listen(3009,()=>{
     console.log('running 3000')
 })
 module.exports = app
